98b6ee228fea69b55646c60518073181d2c252d5
jairo <jairo.vera@kushkipagos.com>
2021-10-27 14:32:16 -0500

    something :v

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import express, { Application, Request, Response, NextFunction } from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+// Boot express
+const app: Application = express();
+const port = 8080;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.raw());
+
+app.post("/hello", async (req: Request, res: Response) => {
+  res.send("Hi there");
+});
+
+app.post("/echo", async (req: Request, res: Response) => {
+  try {
+    const response = await axios.post(
+      "https://api-qa.kushkipagos.click/wallet-reseller/webhook",
+      req.body
+    );
+
+    console.log(response.data);
+
+    res.send(response.data);
+  } catch (e) {
+    console.log(e);
+    res.send("Bypass error");
+  }
+});
+
+// Start server
+app.listen(port, () => console.log(`Server is listening on port ${port}!`));
